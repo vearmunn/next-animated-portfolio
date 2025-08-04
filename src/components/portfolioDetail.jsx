903d2f2928921199615586e2d73fd852d77bd91b
FirmanP <firmanhandipratama2@gmail.com>
2025-08-04 09:03:33 +0700

    images for chat app updated

--- src/components/portfolioDetail.jsx
+++ src/components/portfolioDetail.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import { motion } from 'framer-motion'
+import Image from 'next/image'
 
 const PortofolioDetail = ({ imgUrl, title, descripton, repoUrl }) => {
 
@@ -13,15 +14,15 @@ const PortofolioDetail = ({ imgUrl, title, descripton, repoUrl }) => {
             className='w-full h-full px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 flex flex-col md:flex-row overflow-y-scroll'>
             {/* IMAGE */}
             <div className='w-full md:w-1/2 h-full'>
-                <img src={`../${imgUrl}/main_image.png`} alt="" className='w-full h-full object-contain' />
+                <Image src={`../${imgUrl}/main_image.png`} alt="" className='w-full h-full object-contain' />
             </div>
             {/* TEXT CONTAINER */}
             <div className='w-full md:w-1/2 h-full flex flex-col justify-center gap-5 mt-10 md:mt-0 md:pl-10 lg:pl-12 xl:pl-14'>
                 <h1 className='text-3xl md:text-4xl lg:text-6xl font-bold'>{title}</h1>
                 <p className='text-justify text-sm md:text-base'>{descripton}</p>
-                <button 
-                onClick={()=> window.open(repoUrl)}
-                className='w-full px-4 py-3 border-[1px] border-black md:w-fit rounded-full hover:bg-black hover:text-white'>See Github Repository</button>
+                <button
+                    onClick={() => window.open(repoUrl)}
+                    className='w-full px-4 py-3 border-[1px] border-black md:w-fit rounded-full hover:bg-black hover:text-white'>See Github Repository</button>
                 <div className='mt-3'></div>
             </div>
         </motion.div>
